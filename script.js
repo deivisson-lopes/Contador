9c7b6dcb4ce298c854a60144130ebba5405672c5
Deivisson Lopes <dslvr87@gmail.com>
2023-12-12 18:02:16 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function contar() {
+   var ini = document.getElementById('txti')
+   var fim = document.getElementById('txtf')
+   var passo = document.getElementById('txtp')
+   var res = document.getElementById('res')
+
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+      res.innerHTML = `Impossível contar!`
+      //window.alert('[ERRO] Faltam dados!')
+   } else {
+      res.innerHTML = `Contando: <br>`
+      var i = Number(ini.value)
+      var f = Number(fim.value)
+      var p = Number(passo.value)
+      if (p <= 0 ) {
+         window.alert('Passo inválido! Considerando PASSO 1')
+         p = 1
+      }
+      if(i < f) {
+         // Contagem Crescente
+         for (let c= i; c <= f; c += p) {
+            res.innerHTML += ` 🏴 ${c} `
+         }
+         
+      } else {
+         // Contagem Regressiva
+         for ( let c = i; c >= f; c -= p) {
+            res.innerHTML += `👉 ${c} `
+         }
+      }
+      res.innerHTML += `\u{1f3c1}`
+   } 
+}
+
+
